c51ff72633e4b32edbd131e6c2c9dbef07e5087e
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-02-19 23:52:29 +1100

    Add script to run test C program

---
+++ ctest.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+source=${1-Gemfile}
+
+check() {
+  if wc -c destination | grep -E '^0 ' > /dev/null; then
+    echo 'Copy failed'
+  else
+    echo OK
+  fi
+  echo
+}
+
+printf "FROM debian:10.8-slim\nRUN apt update && apt install -y gcc strace" > /tmp/strace_Dockerfile
+docker build -f /tmp/strace_Dockerfile -t strace .
+echo
+
+echo Local:
+
+(
+  # set -x
+  gcc test.c
+  strace ./a.out "$source" &> test-strace
+)
+check
+
+echo Docker - mounted:
+docker run --rm -v "$(pwd):/work" -w /work strace bash -c "
+  #set -x &&
+  gcc test.c &&
+  strace ./a.out '$source' &> test-strace-docker-mounted
+"
+check
+
+echo Docker - copied:
+docker run --rm -v "$(pwd):/work" -w /work strace bash -c "
+  #set -x &&
+  cp '$source' /x &&
+  gcc test.c &&
+  strace ./a.out /x &> test-strace-docker-copied
+"
+check
