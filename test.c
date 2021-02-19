886af7e59df71b0253f8b2a00dcef5c8efb3ffe5
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-02-20 01:55:56 +1100

    Use open rather than openat for brevity

--- test.c
+++ test.c
@@ -3,16 +3,16 @@
 #include <fcntl.h>
 
 int main(int argc, char *argv[]) {
-  int input = openat(AT_FDCWD, argv[1], O_RDONLY);
-  int temp1 = openat(AT_FDCWD, "/tmp/copy_file_range_test", O_WRONLY|O_CREAT|O_TRUNC, 0100644);
+  int input = open(argv[1], O_RDONLY);
+  int temp1 = open("/tmp/copy_file_range_test", O_WRONLY|O_CREAT|O_TRUNC, 0100644);
 
   syscall(SYS_copy_file_range, input, NULL, temp1, NULL, 6, 0);
 
   close(input);
   close(temp1);
 
-  int temp2 = openat(AT_FDCWD, "/tmp/copy_file_range_test", O_RDONLY);
-  int dest = openat(AT_FDCWD, "destination", O_WRONLY|O_CREAT|O_TRUNC, 0100644);
+  int temp2 = open("/tmp/copy_file_range_test", O_RDONLY);
+  int dest = open("destination", O_WRONLY|O_CREAT|O_TRUNC, 0100644);
 
   syscall(SYS_copy_file_range, temp2, NULL, dest, NULL, 6, 0);
 
